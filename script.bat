79d96a95a45f9cba031604bb9ccab26f8a8cbfd6
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-05-28 09:31:09 +0500

    Final Commit for a working model
    Its finally done!

--- script.bat
+++ script.bat
@@ -3,3 +3,35 @@ title Python Webserver
 echo Python Webserver
 echo created by @dab2020
 timeout 2 >nul
+<con: call "%windir%\system32\cmd.exe" /u/s/c " echo[ Please make sure that index.html is in same directory"
+color 02
+<con: call "%windir%\system32\cmd.exe" /u/s/c " echo[looking for index.html ."
+timeout 1 >nul
+cls
+echo Python Webserver
+echo created by @dab2020
+<con: call "%windir%\system32\cmd.exe" /u/s/c " echo[looking for index.html .."
+timeout 1 >nul
+cls
+echo Python Webserver
+echo created by @dab2020
+<con: call "%windir%\system32\cmd.exe" /u/s/c " echo[looking for index.html ..."
+timeout 1 >nul
+cls
+echo Python Webserver
+echo created by @dab2020
+<con: call "%windir%\system32\cmd.exe" /u/s/c " echo[looking for index.html ..."
+timeout 1 >nul
+cls
+echo Python Webserver
+echo created by @dab2020
+<con: call "%windir%\system32\cmd.exe" /u/s/c " echo[looking for index.html .."
+timeout 1 >nul
+cls echo Python Webserver
+echo created by @dab2020
+<con: call "%windir%\system32\cmd.exe" /u/s/c " echo[looking for index.html ."
+color 07
+cls
+echo Python Webserver
+echo created by @dab2020
+python -m http.server 8080
